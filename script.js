cc59081c051e7e86b4da4b82601d44c3f0c29725
Siyu Liu <siyu.java.web@gmail.com>
2023-11-21 11:57:30 +1100

    edits js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuIcon = document.querySelector(".navbar__menu__hb");
-const menu = document.querySelector('.navbar__menu__list--sm');
-const body = document.querySelector('body');
+const menu = document.querySelector(".navbar__menu__list--sm");
+const body = document.querySelector("body");
 const aboutMe = document.querySelector(".section--about-me");
 const sections = document.querySelectorAll("section");
 const messageBtn = document.querySelector(
@@ -8,43 +8,42 @@ const messageBtn = document.querySelector(
 );
 let isMenuOpen = false;
 
-function clickOnMenuIcon() { 
+function clickOnMenuIcon() {
   isMenuOpen = !isMenuOpen;
   if (isMenuOpen) {
-    menu.style.display = 'block';
-    aboutMe.style.marginTop='280px';
-  } else { 
-    menu.style.display = 'none';
+    menu.style.display = "block";
+    aboutMe.style.marginTop = "280px";
+  } else {
+    menu.style.display = "none";
     aboutMe.style.marginTop = "80px";
   }
 }
-menuIcon.addEventListener('click', clickOnMenuIcon) 
+menuIcon.addEventListener("click", clickOnMenuIcon);
 
 function resizeWindow() {
   console.log(window.innerWidth);
   if (window.innerWidth > 768) {
-    menu.style.display = 'none';
+    menu.style.display = "none";
     aboutMe.style.marginTop = "80px";
-  } 
+  }
 }
-window.addEventListener('resize', resizeWindow)
+window.addEventListener("resize", resizeWindow);
 
-// function clickOnPage() { 
+// function clickOnPage() {
 //   menu.style.display = 'none';
 // }
 // body.addEventListener('click', clickOnPage);
-function sendMessage(event) { 
+function sendMessage(event) {
   event.preventDefault();
   alert(
     "Thank you for sending me a message. I will get in touch with you as soon as possible."
   );
-  
 }
-messageBtn.addEventListener('click', sendMessage)
+messageBtn.addEventListener("click", sendMessage);
 
-sections.forEach((section) => { 
-    section.classList.add('loadin')
-})
+sections.forEach((section) => {
+  section.classList.add("loadin");
+});
 var options = {
   root: null,
   rootMargin: "0px",
@@ -62,4 +61,3 @@ const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((element) => {
   observer.observe(element);
 });
-
